56d2039a2bd01eea42ea7251fcad0f5d5d970d9c
Timoteo Americo <dondeestatimo@gmail.com>
2023-04-19 16:34:54 -0300

    Zoom to raycasthit y camera movement

--- Assets/scripts/cameraMovement.cs
+++ Assets/scripts/cameraMovement.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using DG.Tweening;
 
-public class cameraMovement : MonoBehaviour{
+public class cameraMovement : MonoBehaviour
+{
     Animator myAnim;
     public GameObject camera2D, cameraIso;
     public GameObject center;
@@ -15,14 +16,16 @@ public class cameraMovement : MonoBehaviour{
     public float smoothFactor = 0.5f;
     private bool goingBack;
     private Vector3 newPosition;
-    // Use this for initialization
-    void Start () {
+
+    void Start()
+    {
         myAnim = GetComponent<Animator>();
         cameraOffset = transform.position - center.transform.position;
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         if (Input.GetKeyDown(KeyCode.U))
         {
             cameraTo2d();
@@ -33,19 +36,22 @@ public class cameraMovement : MonoBehaviour{
         }
 
     }
-		
-    public void cameraTo2d() {
+
+    public void cameraTo2d()
+    {
         myAnim.SetBool("ISO", false);
     }
     public void cameraToISO()
     {
         myAnim.SetBool("ISO", true);
     }
-    public void cameraSwitchMode() {
+    public void cameraSwitchMode()
+    {
         myAnim.SetBool("ISO", !myAnim.GetBool("ISO"));
     }
 
-    public void camera2DOff() {
+    public void camera2DOff()
+    {
         camera2D.SetActive(false);
     }
     public void camera2DOn()
@@ -63,12 +69,14 @@ public class cameraMovement : MonoBehaviour{
 
     private void LateUpdate()
     {
-        if (Input.GetMouseButtonDown(1)) {
+        if (Input.GetMouseButtonDown(1))
+        {
 
             movingCamera = !movingCamera;
         }
 
-        if (movingCamera == true) {
+        if (movingCamera == true)
+        {
             Quaternion canTurnAngle = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * speedRotation, Vector3.up);
             cameraOffset = canTurnAngle * cameraOffset;
         }
@@ -77,15 +85,16 @@ public class cameraMovement : MonoBehaviour{
             newPosition = center.transform.position + cameraOffset;
             transform.position = Vector3.Slerp(transform.position, newPosition, smoothFactor);
         }
-        else {
-            transform.DOMove(new Vector3(-8.22f,9.41f,-8.22f), 1f);
-            transform.DORotate(new Vector3(35f,45f,0f),1f);
+        else
+        {
+            transform.DOMove(new Vector3(-8.22f, 9.41f, -8.22f), 1f);
+            //transform.DORotate(new Vector3(35f,45f,0f),1f);
             cameraOffset = transform.position - center.transform.position;
         }
 
         if (movingCamera == true)
         {
-            transform.LookAt(center.transform);
+            //transform.LookAt(center.transform);
         }
 
     }
--- Assets/scripts/panZoom.cs
+++ Assets/scripts/panZoom.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class panZoom : MonoBehaviour {
+public class panZoom : MonoBehaviour
+{
     public Camera MainCamera;
     public float zoomOutMin = 1;
     public float zoomOutMax = 8;
     public Vector3 CameraStartingPosition;
     Touch touchZero, touchOne;
+    public GameObject piece;
+    public Transform pieceTransform;
 
     private void Awake()
     {
@@ -15,12 +18,15 @@ public class panZoom : MonoBehaviour {
     }
 
     // Update is called once per frame
-    void Update()
+    public void Update()
     {
+        Ray ray = MainCamera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        Debug.DrawRay(ray.origin, ray.direction * 100);
         if (Input.touchCount == 2)
         {
-           touchZero = Input.GetTouch(0);
-           touchOne = Input.GetTouch(1);
+            touchZero = Input.GetTouch(0);
+            touchOne = Input.GetTouch(1);
 
             Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
             Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
@@ -30,19 +36,25 @@ public class panZoom : MonoBehaviour {
 
             float difference = currentMagnitude - prevMagnitude;
 
-            zoom(difference * 0.01f);
+            //zoom(difference * 0.01f);
         }
-        //else if (Input.GetMouseButton(0))
-        //{
-        //    Vector3 direction = touchStart - MainCamera.ScreenToWorldPoint(Input.mousePosition);
-        //    Camera.main.transform.position += direction;
-        //}
-        //zoom(Input.GetAxis("Mouse ScrollWheel"));
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(ray, out hit) == true)
+            {
+                Debug.Log("choco con " + hit.transform.name);
+                piece = hit.transform.gameObject;
+                pieceTransform = piece.transform;
+                transform.LookAt(pieceTransform);
+            }
+        }
+        zoom(Input.GetAxis("Mouse ScrollWheel"));
     }
 
     void zoom(float increment)
     {
         MainCamera.orthographicSize = Mathf.Clamp(MainCamera.orthographicSize - increment, zoomOutMin, zoomOutMax);
-        MainCamera.transform.localPosition = new Vector3(MainCamera.transform.localPosition.x+(touchOne.position.x - touchZero.position.x)/2, MainCamera.transform.localPosition.y+(touchOne.position.y - touchZero.position.y)/2,MainCamera.transform.localPosition.z);
+        MainCamera.transform.localPosition = new Vector3(MainCamera.transform.localPosition.x + (touchOne.position.x - touchZero.position.x) / 2, MainCamera.transform.localPosition.y + (touchOne.position.y - touchZero.position.y) / 2, MainCamera.transform.localPosition.z);
+        Debug.Log("Zoom" + increment);
     }
 }
