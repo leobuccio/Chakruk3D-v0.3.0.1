6cbdb06220bcd23b8f0ca04be52de01faf65a827
unknown <diegomolina.a.c@gmail.com>
2023-08-11 10:51:14 -0300

    Options Menu First Version
    Need some sound fixing

---
+++ Assets/scripts/Diego/SC_MusicHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SC_MusicHandler : MonoBehaviour
+{
+    [SerializeField] Slider volumeSlider;
+    [SerializeField] private GameObject Audio;
+    [SerializeField] private AudioSource SceneAudio;
+    [SerializeField] private float soundFactor;
+
+    public void Start()
+    {
+        SceneAudio = Audio.GetComponent<AudioSource>();
+        volumeSlider.value = SceneAudio.volume;
+        
+    }
+
+    public void ChangeVolume ()
+    {
+        SceneAudio.volume = volumeSlider.value * soundFactor;
+    }
+
+}
---
+++ Assets/scripts/Diego/SC_UIHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SC_UIHandler : MonoBehaviour
+{
+    public GameObject rightFrame;
+    public bool isEnabled = true;
+    
+    public void ButtonClicked()
+    {
+        isEnabled = !isEnabled;
+        rightFrame.SetActive(isEnabled);
+    }
+    public void ButtonPointed ()
+    {
+        isEnabled = !isEnabled;
+        rightFrame.SetActive(isEnabled);
+    }
+
+    public void ButtonUnpointed()
+    {
+        isEnabled = !isEnabled;
+        rightFrame.SetActive(isEnabled);
+    }
+}
