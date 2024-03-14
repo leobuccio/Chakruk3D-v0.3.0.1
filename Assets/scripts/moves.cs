d5ae472297afc8db3bc12aa7e0f4a74d96980e26
Diego A. Molina <diegomolina.a.c@gmail.com>
2024-03-14 10:59:02 -0300

    Lore images added

--- Assets/scripts/moves.cs
+++ Assets/scripts/moves.cs
@@ -5,9 +5,23 @@ using UnityEngine.UI;
 
 public class moves : MonoBehaviour {
     public Sprite[] movesSprites;
+    private Sprite currentSprite = null;
+    private bool spriteSetted = false;
+    public GameObject Lore;
     
     public void changeSprite(int a)
     {
-        GetComponent<Image>().sprite = movesSprites[a];
+        GetComponent<Image>().sprite  = movesSprites[a];
+        currentSprite = movesSprites[a];
+        spriteSetted = true;
+    }
+
+    public void ActivateLore()
+    {
+        Lore.GetComponent<SC_UIHandler>().ButtonClicked();
+        if(spriteSetted)
+        {
+            Lore.GetComponent<Image>().sprite = currentSprite;
+        }
     }
 }
