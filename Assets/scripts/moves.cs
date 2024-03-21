6f27eb42fb63d8e5c3d057e6655106f44cb78524
Diego A. Molina <diegomolina.a.c@gmail.com>
2024-03-21 10:52:46 -0300

    Lore text added

--- Assets/scripts/moves.cs
+++ Assets/scripts/moves.cs
@@ -5,13 +5,16 @@ using UnityEngine.UI;
 
 public class moves : MonoBehaviour {
     public Sprite[] movesSprites;
+    public Sprite[] subLoreSprites;
     private Sprite currentSprite = null;
     private bool spriteSetted = false;
     public GameObject Lore;
-    
+    public GameObject SubLore;
+
     public void changeSprite(int a)
     {
         GetComponent<Image>().sprite  = movesSprites[a];
+        SubLore.GetComponent<Image>().sprite = subLoreSprites[a];
         currentSprite = movesSprites[a];
         spriteSetted = true;
     }
